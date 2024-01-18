2d8c1d20a5bc522bb82dc76d1e4d0e261a302954
2shlomi9 <2shlomiariel9@gmail.com>
2024-01-18 14:32:38 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,51 @@
+
+/*#include NumClass.h : NumClass.h file include The declarations of the functions*/
+#include "NumClass.h"
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d   %d", &a, &b);
+    if(a>b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    printf("The Armstrong numbers are:");
+    for (int i = a; i <= b; i++)
+    {
+        if (isArmstrong(i) == 1)
+        {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+    printf("The Palindromes are:");
+    for (int i = a; i <= b; i++)
+    {
+        if (isPalindrome(i) == 1)
+        {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+    printf("The Prime numbers are:");
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i) == 1)
+        {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+    printf("The Strong numbers are:");
+    for (int i = a; i <= b; i++)
+    {
+        if (isStrong(i) == 1)
+        {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
